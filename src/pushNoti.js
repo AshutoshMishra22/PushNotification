80c39738fc2037c73f90e2ab1fafc726d0e4796b
Ashutosh <63530972+AshutoshMishra22@users.noreply.github.com>
2020-04-26 23:42:34 +0530

    PushNotification2

--- src/pushNoti.js
+++ src/pushNoti.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import Push from 'web-push'
-async function subsciber(key){
+async function subsciber(){
   let sw=await navigator.serviceWorker.ready;
   console.log(sw,"sw --")
   let push=sw.pushManager.subscribe({
     userVisibleOnly:true,
-    applicationServerKey:key
+    applicationServerKey:"BBDmJat38JYt-ovOyTyUqP7y0ruHXW7qa3PIkfHKDLeq4zPX9eo9RgNKqAbDg4D2rO7llqgvBgJhs822KFUGcRI"
   })
-  console.log(JSON.stringify(push),"===push---",key)
+  console.log(JSON.stringify(push),"===push---")
 }
-
+// privateKey: "RcacGveLKnCWqjXVzadm3A434lkbzBhZTGg3CpLdhAc"
+// publicKey: "BBDmJat38JYt-ovOyTyUqP7y0ruHXW7qa3PIkfHKDLeq4zPX9eo9RgNKqAbDg4D2rO7llqgvBgJhs822KFUGcRI"
+// __proto__: Object
 const PushNoti =()=>{
-    const keySet=Push.generateVAPIDKeys()
-    //Push.setVapidDetails('mailto:ashutoshmishraofficial22@gmail.com',keys.publicKey,keys.privateKey)
-    console.log(keySet,"KeySet")
+    // const keySet=Push.generateVAPIDKeys()
+    // //Push.setVapidDetails('mailto:ashutoshmishraofficial22@gmail.com',keys.publicKey,keys.privateKey)
+    // console.log(keySet,"KeySet")
 return(
-<button onClick={subsciber(keySet.publicKey)}>Push Me</button>
+<button onClick={subsciber()}>Push Me</button>
 )
 }
 export default PushNoti
