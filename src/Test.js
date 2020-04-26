ae0660f3ebbc06f0538e1f57063b332fbf512051
Ashutosh <63530972+AshutoshMishra22@users.noreply.github.com>
2020-04-26 23:02:50 +0530

    PushNotification2

--- src/Test.js
+++ src/Test.js
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import Push from 'web-push'
 // privateKey: "0rPKYd7Gxrzt4W4eAqm4RSvb4hA9oOca6bcDznAYfkk"
 // publicKey: "BJGaXe-IYZQizmtz4lpqXTi0UNmOcq8dn169YZeayfzv8xCxFvG6ab9WmbLNTVSQiNfCaRI4q-bTCIqrxRt39uc"
@@ -12,6 +12,6 @@ let keys={
 Push.setVapidDetails('mailto:ashutoshmishraofficial22@gmail.com',keys.publicKey,keys.privateKey)
 
 let sub ={
-    
+
 }
 Push.sendNotification(sub,"test Message")
--- src/pushNoti.js
+++ src/pushNoti.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 async function subsciber(){
   let sw=await navigator.serviceWorker.ready;
+  console.log(sw,"sw --")
   let push=sw.pushManager.subscribe({
     userVisibleOnly:true,
     applicationServerKey:"BJGaXe-IYZQizmtz4lpqXTi0UNmOcq8dn169YZeayfzv8xCxFvG6ab9WmbLNTVSQiNfCaRI4q-bTCIqrxRt39uc"
   })
-  console.log(JSON.stringify(push))
+  console.log(JSON.stringify(push),"===push---")
 }
 
 const PushNoti =()=>{
