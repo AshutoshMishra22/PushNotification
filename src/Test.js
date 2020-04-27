88587b7d78b4482bf6b3ea1f97ccc172e6ccf30c
Ashutosh <63530972+AshutoshMishra22@users.noreply.github.com>
2020-04-27 20:04:30 +0530

    trial 1

--- src/Test.js
+++ src/Test.js
@@ -1,21 +1,22 @@
 
-import Push from 'web-push'
-// privateKey: "0rPKYd7Gxrzt4W4eAqm4RSvb4hA9oOca6bcDznAYfkk"
-// publicKey: "BJGaXe-IYZQizmtz4lpqXTi0UNmOcq8dn169YZeayfzv8xCxFvG6ab9WmbLNTVSQiNfCaRI4q-bTCIqrxRt39uc"
+// import Push from 'web-push'
+// // privateKey: "0rPKYd7Gxrzt4W4eAqm4RSvb4hA9oOca6bcDznAYfkk"
+// // publicKey: "BJGaXe-IYZQizmtz4lpqXTi0UNmOcq8dn169YZeayfzv8xCxFvG6ab9WmbLNTVSQiNfCaRI4q-bTCIqrxRt39uc"
 
-let keys={
-    privateKey: "0rPKYd7Gxrzt4W4eAqm4RSvb4hA9oOca6bcDznAYfkk",
-    publicKey: "BJGaXe-IYZQizmtz4lpqXTi0UNmOcq8dn169YZeayfzv8xCxFvG6ab9WmbLNTVSQiNfCaRI4q-bTCIqrxRt39uc"
-}
+// // let keys={
+// //     privateKey: "0rPKYd7Gxrzt4W4eAqm4RSvb4hA9oOca6bcDznAYfkk",
+// //     publicKey: "BJGaXe-IYZQizmtz4lpqXTi0UNmOcq8dn169YZeayfzv8xCxFvG6ab9WmbLNTVSQiNfCaRI4q-bTCIqrxRt39uc"
+// // }
 
 
-const pushme=()=>{
-    const keySet=Push.generateVAPIDKeys()
-    //Push.setVapidDetails('mailto:ashutoshmishraofficial22@gmail.com',keys.publicKey,keys.privateKey)
-    console.log(keySet)
-    // let sub ={
-    return()
-    // }
-    // Push.sendNotification(sub,"test Message")\
-}
-export default pushme
+// const pushme=()=>{
+//     const keySet=Push.generateVAPIDKeys()
+//     //Push.setVapidDetails('mailto:ashutoshmishraofficial22@gmail.com',keys.publicKey,keys.privateKey)
+//     console.log(keySet)
+//     // let sub ={
+//     return(
+//     <div>hii</div>)
+//     // }
+//     // Push.sendNotification(sub,"test Message")\
+// }
+// export default pushme
--- src/pushNoti.js
+++ src/pushNoti.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import Push from 'web-push'
+import Push from 'web-push'
 // async function subsciber(){
 //   let sw=await navigator.serviceWorker.ready;
 //   console.log(sw,"sw --")
@@ -18,36 +18,38 @@ import React from 'react'
     const PushNoti =()=>{
         function onConfirmation(){
             console.log("Granted And .....")
-            var register
-            if ('serviceWorker' in navigator){
-                navigator.serviceWorker.ready
-                .then((reg)=>{
-                    register=reg
-                    //reg.showNotification("Successful TO get Notify By SW")
-                    return reg.pushManager.getSubscription()
-                })
-                .then((sub)=>{
-                    if(sub!==null)
-                    {
-                        console.log("Have subs")
-                    }
-                    else{
-                        console.log(register,"register")
-                        console.log(register.pushManager.subscibe,register.pushManager)
-                        register.pushManager.subscibe({
-                            userVisibleOnly:true,
-                            applicationServerKey:"BBDmJat38JYt-ovOyTyUqP7y0ruHXW7qa3PIkfHKDLeq4zPX9eo9RgNKqAbDg4D2rO7llqgvBgJhs822KFUGcRI"
-                        })
-                    }
-                })
-                .then((newSub)=>{
-                    console.log(newSub,"New Subscription")
-                })
-            }
-            else{
-                console.log("No service Worker")
-                new Notification("Successful TO get Notify")
-            }
+            //var register
+            const keySet=Push.generateVAPIDKeys()
+            console.log(keySet)
+            // if ('serviceWorker' in navigator){
+            //     navigator.serviceWorker.ready
+            //     .then((reg)=>{
+            //         register=reg
+            //         //reg.showNotification("Successful TO get Notify By SW")
+            //         return reg.pushManager.getSubscription()
+            //     })
+                // .then((sub)=>{
+                //     if(sub!==null)
+                //     {
+                //         console.log("Have subs")
+                //     }
+                //     else{
+                //         console.log(register,"register")
+                //         console.log(register.pushManager.subscibe,register.pushManager)
+                //         register.pushManager.subscibe({
+                //             userVisibleOnly:true,
+                //             applicationServerKey:""
+                //         })
+                //     }
+                // })
+                // .then((newSub)=>{
+                //     console.log(newSub,"New Subscription")
+                // })
+            // }
+            // else{
+            //     console.log("No service Worker")
+            //     new Notification("Successful TO get Notify")
+            // }
             
         }
 
