9a66b94f14d85898501ca631de76a20e6c62046a
Ashutosh <63530972+AshutoshMishra22@users.noreply.github.com>
2020-04-27 20:17:18 +0530

    trial 1

--- src/pushNoti.js
+++ src/pushNoti.js
@@ -9,47 +9,47 @@ import Push from 'web-push'
 //   })
 //   console.log(JSON.stringify(push),"===push---")
 // }
-// // privateKey: "RcacGveLKnCWqjXVzadm3A434lkbzBhZTGg3CpLdhAc"
-// // publicKey: "BBDmJat38JYt-ovOyTyUqP7y0ruHXW7qa3PIkfHKDLeq4zPX9eo9RgNKqAbDg4D2rO7llqgvBgJhs822KFUGcRI"
-// // __proto__: Object
+// privateKey: "eWtysixaaPGACFPSQgWFSDtEr9NLPimus-zjPcyvBcE"
+// publicKey: "BGm_wqYnJmPHV_AV1NAB-xMvQ6AUkX4zNwxgGMRUCQtRT6FMEABHszRYG0ibMwy36l64mx5uKsfCO_DRHmvlT-A"
+// __proto__: Object
 // const keySet=Push.generateVAPIDKeys()
     // //Push.setVapidDetails('mailto:ashutoshmishraofficial22@gmail.com',keys.publicKey,keys.privateKey)
 
     const PushNoti =()=>{
         function onConfirmation(){
             console.log("Granted And .....")
-            //var register
+            var register
             const keySet=Push.generateVAPIDKeys()
             console.log(keySet)
-            // if ('serviceWorker' in navigator){
-            //     navigator.serviceWorker.ready
-            //     .then((reg)=>{
-            //         register=reg
-            //         //reg.showNotification("Successful TO get Notify By SW")
-            //         return reg.pushManager.getSubscription()
-            //     })
-                // .then((sub)=>{
-                //     if(sub!==null)
-                //     {
-                //         console.log("Have subs")
-                //     }
-                //     else{
-                //         console.log(register,"register")
-                //         console.log(register.pushManager.subscibe,register.pushManager)
-                //         register.pushManager.subscibe({
-                //             userVisibleOnly:true,
-                //             applicationServerKey:""
-                //         })
-                //     }
-                // })
-                // .then((newSub)=>{
-                //     console.log(newSub,"New Subscription")
-                // })
-            // }
-            // else{
-            //     console.log("No service Worker")
-            //     new Notification("Successful TO get Notify")
-            // }
+            if ('serviceWorker' in navigator){
+                navigator.serviceWorker.ready
+                .then((reg)=>{
+                    register=reg
+                    //reg.showNotification("Successful TO get Notify By SW")
+                    return reg.pushManager.getSubscription()
+                })
+                .then((sub)=>{
+                    if(sub!==null)
+                    {
+                        console.log("Have subs")
+                    }
+                    else{
+                        console.log(register,"register")
+                        console.log(register.pushManager.subscibe,register.pushManager)
+                        register.pushManager.subscibe({
+                            userVisibleOnly:true,
+                            applicationServerKey:"BGm_wqYnJmPHV_AV1NAB-xMvQ6AUkX4zNwxgGMRUCQtRT6FMEABHszRYG0ibMwy36l64mx5uKsfCO_DRHmvlT-A"
+                        })
+                    }
+                })
+                .then((newSub)=>{
+                    console.log(newSub,"New Subscription")
+                })
+            }
+            else{
+                console.log("No service Worker")
+                new Notification("Successful TO get Notify")
+            }
             
         }
 
