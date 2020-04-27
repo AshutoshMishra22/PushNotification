9957abc6b603ecd2c01dfc0dd78e20be9c1c6f6c
Ashutosh <63530972+AshutoshMishra22@users.noreply.github.com>
2020-04-28 00:18:33 +0530

    somehow

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import React, { useEffect } from 'react';
 import PushNoti from './pushNoti'
 import Webpush from 'web-push'
 function App() {
-// useEffect(()=>{
+useEffect(()=>{
 //   const keys={privateKey: "eWtysixaaPGACFPSQgWFSDtEr9NLPimus-zjPcyvBcE",
 // publicKey: "BGm_wqYnJmPHV_AV1NAB-xMvQ6AUkX4zNwxgGMRUCQtRT6FMEABHszRYG0ibMwy36l64mx5uKsfCO_DRHmvlT-A"
 //   }
-//   Webpush.setVapidDetails('mailto:ankur1998mishra@gmail.com',keys.publicKey,keys.privateKey)
-//   var pushConfig={endpoint:"https://fcm.googleapis.com/fcm/send/cJ0TkS8A7RA:APA91bGHRMau9eQK4LV8Eapcu_cqoO9hMKNe6ZzK9tHqdauMIBlpvnxRew_6uodPK0bfQnoV6UWfss4-qU1dfBzqHg7CKm1fjfIKyCcxOm2HfvvEauyTAcNOw8I8y1TxXWu0ZgYYaXmr",
-//   expirationTime:null,
-//   keys:{
-//     p256dh:"BI9nQMqoj4SyJfaqvInyxmDYqQW3fYYF-eJl08q4GNZ5sDW-A02E5v08Ci_EakKUPYox3QsvzrgMhn3Hg4uEzsg",
-//     auth:"kOnk7sOdgb1TF3STRt7g1Q"
-//   }}
-//   console.log(pushConfig,"pushme pls")
-//   Webpush.sendNotification(pushConfig,"hiii")
-//   .catch((err)=>{
-//     console.log(err,"this error occured")
-//   })
-// },[])
+// Webpush.setVapidDetails('mailto:ankur1998mishra@gmail.com',"BGm_wqYnJmPHV_AV1NAB-xMvQ6AUkX4zNwxgGMRUCQtRT6FMEABHszRYG0ibMwy36l64mx5uKsfCO_DRHmvlT-A", "eWtysixaaPGACFPSQgWFSDtEr9NLPimus-zjPcyvBcE")
+// var pushConfig={endpoint:"https://fcm.googleapis.com/fcm/send/cJ0TkS8A7RA:APA91bGHRMau9eQK4LV8Eapcu_cqoO9hMKNe6ZzK9tHqdauMIBlpvnxRew_6uodPK0bfQnoV6UWfss4-qU1dfBzqHg7CKm1fjfIKyCcxOm2HfvvEauyTAcNOw8I8y1TxXWu0ZgYYaXmr",
+// expirationTime:null,
+// keys:{
+//   p256dh:"BI9nQMqoj4SyJfaqvInyxmDYqQW3fYYF-eJl08q4GNZ5sDW-A02E5v08Ci_EakKUPYox3QsvzrgMhn3Hg4uEzsg",
+//   auth:"kOnk7sOdgb1TF3STRt7g1Q"
+// }}
+// console.log(pushConfig,"pushme pls")
+// Webpush.sendNotification(pushConfig,"Yesssssssssssssssssss")
+// .catch((err)=>{
+//   console.log(err,"this error occured",Webpush)
+// })
+},[])
   return (
     <div className="App">
       <PushNoti/>
--- src/Test.js
+++ src/Test.js
@@ -1,22 +1,14 @@
 
-// import Push from 'web-push'
-// // privateKey: "0rPKYd7Gxrzt4W4eAqm4RSvb4hA9oOca6bcDznAYfkk"
-// // publicKey: "BJGaXe-IYZQizmtz4lpqXTi0UNmOcq8dn169YZeayfzv8xCxFvG6ab9WmbLNTVSQiNfCaRI4q-bTCIqrxRt39uc"
-
-// // let keys={
-// //     privateKey: "0rPKYd7Gxrzt4W4eAqm4RSvb4hA9oOca6bcDznAYfkk",
-// //     publicKey: "BJGaXe-IYZQizmtz4lpqXTi0UNmOcq8dn169YZeayfzv8xCxFvG6ab9WmbLNTVSQiNfCaRI4q-bTCIqrxRt39uc"
-// // }
-
-
-// const pushme=()=>{
-//     const keySet=Push.generateVAPIDKeys()
-//     //Push.setVapidDetails('mailto:ashutoshmishraofficial22@gmail.com',keys.publicKey,keys.privateKey)
-//     console.log(keySet)
-//     // let sub ={
-//     return(
-//     <div>hii</div>)
-//     // }
-//     // Push.sendNotification(sub,"test Message")\
-// }
-// export default pushme
+const Webpush=require('web-push')
+Webpush.setVapidDetails('mailto:ankur1998mishra@gmail.com',"BGm_wqYnJmPHV_AV1NAB-xMvQ6AUkX4zNwxgGMRUCQtRT6FMEABHszRYG0ibMwy36l64mx5uKsfCO_DRHmvlT-A", "eWtysixaaPGACFPSQgWFSDtEr9NLPimus-zjPcyvBcE")
+  var pushConfig={endpoint:"https://fcm.googleapis.com/fcm/send/cJ0TkS8A7RA:APA91bGHRMau9eQK4LV8Eapcu_cqoO9hMKNe6ZzK9tHqdauMIBlpvnxRew_6uodPK0bfQnoV6UWfss4-qU1dfBzqHg7CKm1fjfIKyCcxOm2HfvvEauyTAcNOw8I8y1TxXWu0ZgYYaXmr",
+  expirationTime:null,
+  keys:{
+    p256dh:"BI9nQMqoj4SyJfaqvInyxmDYqQW3fYYF-eJl08q4GNZ5sDW-A02E5v08Ci_EakKUPYox3QsvzrgMhn3Hg4uEzsg",
+    auth:"kOnk7sOdgb1TF3STRt7g1Q"
+  }}
+  console.log(pushConfig,"pushme pls")
+  Webpush.sendNotification(pushConfig,"Finally we done it ")
+  .catch((err)=>{
+    console.log(err,"this error occured")
+  })
